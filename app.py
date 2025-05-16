fb635fb72795c5db2c2056c307b4126611a8a9e0
Garvit Singh <garvitsinghgit@gmail.com>
2025-05-17 00:11:55 +0530

    Final Changes Done

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from db import DataBase
+from textblob import TextBlob
 import json
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
 app = Flask(__name__)
 
 obj = DataBase()
@@ -51,6 +54,7 @@ def welcome():
 
 
 # -------------------------------------------------------------
+# Name Entity Recognition
 
 
 @app.route("/ner")
@@ -58,14 +62,23 @@ def ner():
     return render_template("ner.html")
 
 
-@app.route("/perform_ner", methods=["post"])
+def extract_entities(text):
+    doc = nlp(text)
+    return [{"entity": ent.text, "label": ent.label_} for ent in doc.ents]
+
+
+@app.route("/perform_ner", methods=["POST"])
 def perform_ner():
     text = request.form.get("text")
-    response = obj.ner(text)
-    return render_template("ner.html", response=response)
+    entities = extract_entities(text)
+    for i in range(len(entities)):
+        print(entities[i]["entity"], entities[i]["label"])
+    return render_template("ner.html", response=entities)
 
 
 # ------------------------------------------------------------
+
+# Sentiment Analysis
 
 
 @app.route("/sentiment_analysis")
@@ -73,14 +86,33 @@ def sentiment_analysis():
     return render_template("sentiment_analysis.html")
 
 
+def extract_sentiment(text):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+
+    sentiment = (
+        "Positive" if polarity > 0 else "Negative" if polarity < 0 else "Neutral"
+    )
+
+    return {
+        "sentiment": sentiment,
+        "polarity": round(polarity, 2),
+        "subjectivity": round(subjectivity, 2),
+    }
+
+
 @app.route("/perform_sentiment_analysis", methods=["post"])
 def perform_sentiment_analysis():
     text = request.form.get("text")
-    response = obj.sentiment_analysis(text)
+    response = extract_sentiment(text)
+    print(response)
     return render_template("sentiment_analysis.html", response=response)
 
 
 # ---------------------------------------------------------------------------
+
+# abuse detection
 
 
 @app.route("/abuse_detection")
@@ -88,10 +120,37 @@ def abuse_detection():
     return render_template("abuse_detection.html")
 
 
+def extract_abuse_detection(text):
+    abusive_keywords = {
+        "idiot",
+        "stupid",
+        "dumb",
+        "nonsense",
+        "hate",
+        "kill",
+        "shit",
+        "bastard",
+        "asshole",
+        "moron",
+        "fool",
+        "bloody",
+        "bitch",
+        "crap",
+        "fuck",
+        "retard",
+        "suck",
+        "damn",
+    }
+    found = [word for word in text.lower().split() if word in abusive_keywords]
+    is_abusive = len(found) > 0
+
+    return {"is_abusive": is_abusive, "bad_words": found}
+
+
 @app.route("/perform_abuse_detection", methods=["post"])
 def perform_abuse_detection():
     text = request.form.get("text")
-    response = obj.abuse_detection(text)
+    response = extract_abuse_detection(text)
     return render_template("abuse_detection.html", response=response)
 
 
